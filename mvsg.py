9d077498adcd055d50e841a04a794d2642715679
Eric Pheatt <epheatt@servicesource.com>
2015-04-21 13:51:55 -0700

    restore parallel fetch using Pool and return lists

--- mvsg.py
+++ mvsg.py
@@ -4,7 +4,7 @@ import json
 import sys
 import time
 import urllib2
-from multiprocessing import Pool
+from multiprocessing import Pool, Queue
 
 if len(sys.argv) != 7:
     sys.stderr.write('Not enough (or too many) arguments\n')
@@ -25,8 +25,12 @@ timestamp = timestamp_millis / 1000
 def escape_core(str):
     return str.replace('.','_')
 
-def dispatch_value(core_name, field, value, ts):
-    print '{0}.{1}.{2} {3} {4}'.format(prefix, escape_core(core_name), field, value, ts)
+def dispatch_value(core_name, field, value, ts, q=None):
+    str = '{0}.{1}.{2} {3} {4}'.format(prefix, escape_core(core_name), field, value, ts)
+    if q is not None:
+        q.append(str)
+    else:
+        print str
 
 def request_and_response_or_bail(method, url, message):
     try:
@@ -69,90 +73,90 @@ def system_stats(core_name, omit_jvm_stats):
     print '{0}.{1} {2} {3}'.format(prefix, 'system.totalPhysicalMemorySize', system_json['system']['totalPhysicalMemorySize'], timestamp)
     print '{0}.{1} {2} {3}'.format(prefix, 'system.totalSwapSpaceSize', system_json['system']['totalSwapSpaceSize'], timestamp)
 
-def query_handler_stats(stats, core_name, name, ts):
-    dispatch_value(core_name, '{0}.5minRateReqsPerSecond'.format(name), stats['5minRateReqsPerSecond'], ts)
-    dispatch_value(core_name, '{0}.15minRateReqsPerSecond'.format(name), stats['15minRateReqsPerSecond'], ts)
-    dispatch_value(core_name, '{0}.75thPcRequestTime'.format(name), stats['75thPcRequestTime'], ts)
-    dispatch_value(core_name, '{0}.95thPcRequestTime'.format(name), stats['95thPcRequestTime'], ts)
-    dispatch_value(core_name, '{0}.999thPcRequestTime'.format(name), stats['999thPcRequestTime'], ts)
-    dispatch_value(core_name, '{0}.99thPcRequestTime'.format(name), stats['99thPcRequestTime'], ts)
-    dispatch_value(core_name, '{0}.avgRequestsPerSecond'.format(name), stats['avgRequestsPerSecond'], ts)
-    dispatch_value(core_name, '{0}.avgTimePerRequest'.format(name), stats['avgTimePerRequest'], ts)
-    dispatch_value(core_name, '{0}.errors'.format(name), stats['errors'], ts)
-    dispatch_value(core_name, '{0}.handlerStart'.format(name), stats['handlerStart'], ts)
-    dispatch_value(core_name, '{0}.medianRequestTime'.format(name), stats['medianRequestTime'], ts)
-    dispatch_value(core_name, '{0}.requests'.format(name), stats['requests'], ts)
-    dispatch_value(core_name, '{0}.timeouts'.format(name), stats['timeouts'], ts)
-    dispatch_value(core_name, '{0}.totalTime'.format(name), stats['totalTime'], ts)
+def query_handler_stats(stats, core_name, name, ts, q=None):
+    dispatch_value(core_name, '{0}.5minRateReqsPerSecond'.format(name), stats['5minRateReqsPerSecond'], ts, q)
+    dispatch_value(core_name, '{0}.15minRateReqsPerSecond'.format(name), stats['15minRateReqsPerSecond'], ts, q)
+    dispatch_value(core_name, '{0}.75thPcRequestTime'.format(name), stats['75thPcRequestTime'], ts, q)
+    dispatch_value(core_name, '{0}.95thPcRequestTime'.format(name), stats['95thPcRequestTime'], ts, q)
+    dispatch_value(core_name, '{0}.999thPcRequestTime'.format(name), stats['999thPcRequestTime'], ts, q)
+    dispatch_value(core_name, '{0}.99thPcRequestTime'.format(name), stats['99thPcRequestTime'], ts, q)
+    dispatch_value(core_name, '{0}.avgRequestsPerSecond'.format(name), stats['avgRequestsPerSecond'], ts, q)
+    dispatch_value(core_name, '{0}.avgTimePerRequest'.format(name), stats['avgTimePerRequest'], ts, q)
+    dispatch_value(core_name, '{0}.errors'.format(name), stats['errors'], ts, q)
+    dispatch_value(core_name, '{0}.handlerStart'.format(name), stats['handlerStart'], ts, q)
+    dispatch_value(core_name, '{0}.medianRequestTime'.format(name), stats['medianRequestTime'], ts, q)
+    dispatch_value(core_name, '{0}.requests'.format(name), stats['requests'], ts, q)
+    dispatch_value(core_name, '{0}.timeouts'.format(name), stats['timeouts'], ts, q)
+    dispatch_value(core_name, '{0}.totalTime'.format(name), stats['totalTime'], ts, q)
 
-def update_handler_stats(stats, core_name, name, ts):
-    dispatch_value(core_name, '{0}.adds'.format(name), stats['adds'], ts)
-    dispatch_value(core_name, '{0}.autoCommits'.format(name), stats['autocommits'], ts)
-    dispatch_value(core_name, '{0}.commits'.format(name), stats['commits'], ts)
-    dispatch_value(core_name, '{0}.cumulativeAdds'.format(name), stats['cumulative_adds'], ts)
-    dispatch_value(core_name, '{0}.cumulativeDeletesById'.format(name), stats['cumulative_deletesById'], ts)
-    dispatch_value(core_name, '{0}.cumulativeDeletesByQuery'.format(name), stats['cumulative_deletesByQuery'], ts)
-    dispatch_value(core_name, '{0}.cumulativeErrors'.format(name), stats['cumulative_errors'], ts)
-    dispatch_value(core_name, '{0}.deletesById'.format(name), stats['deletesById'], ts)
-    dispatch_value(core_name, '{0}.deletesByQuery'.format(name), stats['deletesByQuery'], ts)
-    dispatch_value(core_name, '{0}.docsPending'.format(name), stats['docsPending'], ts)
-    dispatch_value(core_name, '{0}.errors'.format(name), stats['errors'], ts)
-    dispatch_value(core_name, '{0}.expungeDeletes'.format(name), stats['expungeDeletes'], ts)
-    dispatch_value(core_name, '{0}.optimizes'.format(name), stats['optimizes'], ts)
-    dispatch_value(core_name, '{0}.rollbacks'.format(name), stats['rollbacks'], ts)
-    dispatch_value(core_name, '{0}.softAutoCommits'.format(name), stats['soft autocommits'], ts)
-    dispatch_value(core_name, '{0}.transactionLogsTotalNumber'.format(name), stats['transaction_logs_total_number'], ts)
-    dispatch_value(core_name, '{0}.transactionLogsTotalSize'.format(name), stats['transaction_logs_total_size'], ts)
+def update_handler_stats(stats, core_name, name, ts, q=None):
+    dispatch_value(core_name, '{0}.adds'.format(name), stats['adds'], ts, q)
+    dispatch_value(core_name, '{0}.autoCommits'.format(name), stats['autocommits'], ts, q)
+    dispatch_value(core_name, '{0}.commits'.format(name), stats['commits'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeAdds'.format(name), stats['cumulative_adds'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeDeletesById'.format(name), stats['cumulative_deletesById'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeDeletesByQuery'.format(name), stats['cumulative_deletesByQuery'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeErrors'.format(name), stats['cumulative_errors'], ts, q)
+    dispatch_value(core_name, '{0}.deletesById'.format(name), stats['deletesById'], ts, q)
+    dispatch_value(core_name, '{0}.deletesByQuery'.format(name), stats['deletesByQuery'], ts, q)
+    dispatch_value(core_name, '{0}.docsPending'.format(name), stats['docsPending'], ts, q)
+    dispatch_value(core_name, '{0}.errors'.format(name), stats['errors'], ts, q)
+    dispatch_value(core_name, '{0}.expungeDeletes'.format(name), stats['expungeDeletes'], ts, q)
+    dispatch_value(core_name, '{0}.optimizes'.format(name), stats['optimizes'], ts, q)
+    dispatch_value(core_name, '{0}.rollbacks'.format(name), stats['rollbacks'], ts, q)
+    dispatch_value(core_name, '{0}.softAutoCommits'.format(name), stats['soft autocommits'], ts, q)
+    dispatch_value(core_name, '{0}.transactionLogsTotalNumber'.format(name), stats['transaction_logs_total_number'], ts, q)
+    dispatch_value(core_name, '{0}.transactionLogsTotalSize'.format(name), stats['transaction_logs_total_size'], ts, q)
 
-def cache_stats(stats, core_name, name, ts):
-    dispatch_value(core_name, '{0}.cumulativeEvictions'.format(name), stats['cumulative_evictions'], ts)
-    dispatch_value(core_name, '{0}.cumulativeHitRatio'.format(name), stats['cumulative_hitratio'], ts)
-    dispatch_value(core_name, '{0}.cumulativeHits'.format(name), stats['cumulative_hits'], ts)
-    dispatch_value(core_name, '{0}.cumulativeInserts'.format(name), stats['cumulative_inserts'], ts)
-    dispatch_value(core_name, '{0}.cumulativeLookups'.format(name), stats['cumulative_lookups'], ts)
-    dispatch_value(core_name, '{0}.evictions'.format(name), stats['evictions'], ts)
-    dispatch_value(core_name, '{0}.hitRatio'.format(name), stats['hitratio'], ts)
-    dispatch_value(core_name, '{0}.hits'.format(name), stats['hits'], ts)
-    dispatch_value(core_name, '{0}.inserts'.format(name), stats['inserts'], ts)
-    dispatch_value(core_name, '{0}.lookups'.format(name), stats['lookups'], ts)
-    dispatch_value(core_name, '{0}.size'.format(name), stats['size'], ts)
-    dispatch_value(core_name, '{0}.warmupTime'.format(name), stats['warmupTime'], ts)
+def cache_stats(stats, core_name, name, ts, q=None):
+    dispatch_value(core_name, '{0}.cumulativeEvictions'.format(name), stats['cumulative_evictions'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeHitRatio'.format(name), stats['cumulative_hitratio'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeHits'.format(name), stats['cumulative_hits'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeInserts'.format(name), stats['cumulative_inserts'], ts, q)
+    dispatch_value(core_name, '{0}.cumulativeLookups'.format(name), stats['cumulative_lookups'], ts, q)
+    dispatch_value(core_name, '{0}.evictions'.format(name), stats['evictions'], ts, q)
+    dispatch_value(core_name, '{0}.hitRatio'.format(name), stats['hitratio'], ts, q)
+    dispatch_value(core_name, '{0}.hits'.format(name), stats['hits'], ts, q)
+    dispatch_value(core_name, '{0}.inserts'.format(name), stats['inserts'], ts, q)
+    dispatch_value(core_name, '{0}.lookups'.format(name), stats['lookups'], ts, q)
+    dispatch_value(core_name, '{0}.size'.format(name), stats['size'], ts, q)
+    dispatch_value(core_name, '{0}.warmupTime'.format(name), stats['warmupTime'], ts, q)
 
 def core_stats(core_name):
+    q = list()
     ts_millis = int(round(time.time() * 1000))
     ts = ts_millis / 1000
     mbeans_content = request_and_response_or_bail('GET', '/solr/{0}/admin/mbeans?stats=true&wt=json&_={1}'.format(core_name, ts_millis), 'Error while retrieving core stats')
     mbeans_json = get_mbeans(json.loads(mbeans_content))
     searcher_stats = mbeans_json['CORE']['searcher']['stats']
-    dispatch_value(core_name, 'numDocs', searcher_stats['numDocs'], ts)
-    dispatch_value(core_name, 'maxDoc', searcher_stats['maxDoc'], ts)
+    dispatch_value(core_name, 'numDocs', searcher_stats['numDocs'], ts, q)
+    dispatch_value(core_name, 'maxDoc', searcher_stats['maxDoc'], ts, q)
     if 'numSegments' in searcher_stats:
-        dispatch_value(core_name, 'numSegments', searcher_stats['numSegments'], ts)
-    dispatch_value(core_name, 'deletedDocs', searcher_stats['deletedDocs'], ts)
-    dispatch_value(core_name, 'indexVersion', searcher_stats['indexVersion'], ts)
-    dispatch_value(core_name, 'warmupTime', searcher_stats['warmupTime'], ts)
+        dispatch_value(core_name, 'numSegments', searcher_stats['numSegments'], ts, q)
+    dispatch_value(core_name, 'deletedDocs', searcher_stats['deletedDocs'], ts, q)
+    dispatch_value(core_name, 'indexVersion', searcher_stats['indexVersion'], ts, q)
+    dispatch_value(core_name, 'warmupTime', searcher_stats['warmupTime'], ts, q)
     core_stats = mbeans_json['CORE']['core']['stats']
-    dispatch_value(core_name, 'refCount', core_stats['refCount'], ts)
-    #query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.component.SearchHandler']['stats'], core_name, 'search', ts)
-    #query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.UpdateRequestHandler']['stats'], core_name, 'updateRequest, ts)
-    query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.ReplicationHandler']['stats'], core_name, 'replication', ts)
-    dispatch_value(core_name, 'replication.indexVersion', mbeans_json['QUERYHANDLER']['org.apache.solr.handler.ReplicationHandler']['stats']['indexVersion'], ts)
-    dispatch_value(core_name, 'replication.generation', mbeans_json['QUERYHANDLER']['org.apache.solr.handler.ReplicationHandler']['stats']['generation'], ts)
-    query_handler_stats(mbeans_json['QUERYHANDLER']['/select']['stats'], core_name, 'select', ts)
-    query_handler_stats(mbeans_json['QUERYHANDLER']['/update']['stats'], core_name, 'update', ts)
-    #query_handler_stats(mbeans_json['QUERYHANDLER']['dismax']['stats'], core_name, 'dismax', ts)
-    #query_handler_stats(mbeans_json['QUERYHANDLER']['standard']['stats'], core_name, 'standard', ts)
-    #query_handler_stats(mbeans_json['QUERYHANDLER']['warming']['stats'], core_name, 'warming', ts)
-    query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.UpdateRequestHandler']['stats'], core_name, 'updateRequest', ts)
-    update_handler_stats(mbeans_json['UPDATEHANDLER']['updateHandler']['stats'], core_name, 'updateHandler', ts)
-    cache_stats(mbeans_json['CACHE']['documentCache']['stats'], core_name, 'documentCache', ts)
-    cache_stats(mbeans_json['CACHE']['fieldValueCache']['stats'], core_name, 'fieldValueCache', ts)
-    cache_stats(mbeans_json['CACHE']['filterCache']['stats'], core_name, 'filterCache', ts)
+    dispatch_value(core_name, 'refCount', core_stats['refCount'], ts, q)
+    #query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.component.SearchHandler']['stats'], core_name, 'search', ts, q)
+    #query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.UpdateRequestHandler']['stats'], core_name, 'updateRequest, ts, q)
+    query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.ReplicationHandler']['stats'], core_name, 'replication', ts, q)
+    dispatch_value(core_name, 'replication.indexVersion', mbeans_json['QUERYHANDLER']['org.apache.solr.handler.ReplicationHandler']['stats']['indexVersion'], ts, q)
+    dispatch_value(core_name, 'replication.generation', mbeans_json['QUERYHANDLER']['org.apache.solr.handler.ReplicationHandler']['stats']['generation'], ts, q)
+    query_handler_stats(mbeans_json['QUERYHANDLER']['/select']['stats'], core_name, 'select', ts, q)
+    query_handler_stats(mbeans_json['QUERYHANDLER']['/update']['stats'], core_name, 'update', ts, q)
+    query_handler_stats(mbeans_json['QUERYHANDLER']['org.apache.solr.handler.UpdateRequestHandler']['stats'], core_name, 'updateRequest', ts, q)
+    update_handler_stats(mbeans_json['UPDATEHANDLER']['updateHandler']['stats'], core_name, 'updateHandler', ts, q)
+    cache_stats(mbeans_json['CACHE']['documentCache']['stats'], core_name, 'documentCache', ts, q)
+    cache_stats(mbeans_json['CACHE']['fieldValueCache']['stats'], core_name, 'fieldValueCache', ts, q)
+    cache_stats(mbeans_json['CACHE']['filterCache']['stats'], core_name, 'filterCache', ts, q)
     if 'nCache' in mbeans_json['CACHE']:
-        cache_stats(mbeans_json['CACHE']['nCache']['stats'], core_name, 'nCache', ts)
-    cache_stats(mbeans_json['CACHE']['queryResultCache']['stats'], core_name, 'queryResultCache', ts)
-    dispatch_value(core_name, 'fieldCache.entriesCount', mbeans_json['CACHE']['fieldCache']['stats']['entries_count'], ts)
-    dispatch_value(core_name, 'fieldCache.insanityCount', mbeans_json['CACHE']['fieldCache']['stats']['insanity_count'], ts)
+        cache_stats(mbeans_json['CACHE']['nCache']['stats'], core_name, 'nCache', ts, q)
+    cache_stats(mbeans_json['CACHE']['perSegFilter']['stats'], core_name, 'perSegFilter', ts, q)
+    cache_stats(mbeans_json['CACHE']['queryResultCache']['stats'], core_name, 'queryResultCache', ts, q)
+    dispatch_value(core_name, 'fieldCache.entriesCount', mbeans_json['CACHE']['fieldCache']['stats']['entries_count'], ts, q)
+    dispatch_value(core_name, 'fieldCache.insanityCount', mbeans_json['CACHE']['fieldCache']['stats']['insanity_count'], ts, q)
+    return q
 
 cores_content = request_and_response_or_bail('GET', '/solr/admin/cores?wt=json&indexInfo=true&_={0}'.format(timestamp_millis), 'Error while retrieving cores.')
 cores_json = json.loads(cores_content)
@@ -171,8 +175,13 @@ for core_name in core_names:
     dispatch_value(core_name, 'indexHeapUsageBytes', cores_json['status'][core_name]['index']['indexHeapUsageBytes'], timestamp)
     dispatch_value(core_name, 'sizeInBytes', cores_json['status'][core_name]['index']['sizeInBytes'], timestamp)
     dispatch_value(core_name, 'segmentCount', cores_json['status'][core_name]['index']['segmentCount'], timestamp)
-    core_stats(core_name)
+    #results = core_stats(core_name)
+    #for q in results:
+    #    print q
 
-#if __name__ == '__main__':
-#    p = Pool(len(core_names))
-#    p.map(core_stats, core_names)
+if __name__ == '__main__':
+    p = Pool(len(core_names))
+    results = p.map(core_stats, core_names)
+    for q in results:
+        for i in q:
+            print i
